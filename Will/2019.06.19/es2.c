c0687996e3f09e8cd1bc49d5209bca2735342ae2
WilliamArnone <arnonewilliam@gmail.com>
2020-05-27 11:57:07 +0200

    Will esercizi pt.2

--- Will/2019.06.19/es2.c
+++ Will/2019.06.19/es2.c
@@ -1,10 +1,5 @@
-// Normalmente se un processo è orfano l'exit status viene ricevuto da init o systemd (processo 1).
-// Usando l'opzione PR_SET_CHILD_SUBREAPER della system call prctl(2) è possibile cambiare questo
-// comportamento.
-// Scrivere un programma che crei un processo figlio e uno nipote (tramite fork). Fare in modo che
-// termini prima il figlo e poi il nipote.
-// Il programma deve mostrare che con prctl/PR_SET_CHILD_SUBREAPER la terminazione del nipote
-// viene rilevata dal nonno.
+// Tramite l'opzione PR_SET_NAME sempre di prctl fare in modo che con il comando "ps -Leo pid,comm"
+// i nomi dei tre rpocessi creati dall'esercizio 1 compaiano con nome "nonno","figlio" e "nipote".
 
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -21,6 +16,7 @@ int main (int argc, char * argv[]){
     char buff;
     pipe(pipefd);
     pid_t child, grandchild;
+    char nome[16];
 
     prctl(PR_SET_CHILD_SUBREAPER);
 
@@ -30,15 +26,20 @@ int main (int argc, char * argv[]){
         grandchild=fork();
 
         if(grandchild==0){
-            for(long i=0; i<1000000; i++);
+            for(long i=0; i<10000000; i++);
+            strcpy(nome,"nipote");
+            prctl(PR_SET_NAME, nome);
         }else{
+            strcpy(nome,"figlio");
+            prctl(PR_SET_NAME, nome);
             close(pipefd[0]); 
             write(pipefd[1], &grandchild, sizeof(pid_t));
             close(pipefd[1]);  
         }
         
     }else{
-        
+        strcpy(nome,"nonno");
+        prctl(PR_SET_NAME, nome);
         if(wait(NULL)==child)
             printf("Child dead\n");
 
@@ -47,7 +48,7 @@ int main (int argc, char * argv[]){
         close(pipefd[0]);
 
         if(wait(NULL)==grandchild)
-            printf("Grandchild dead\n");
+            printf("Grandchild dead   %d\n",getpid());
     }
     
 
--- Will/2019.07.16/es2.c
+++
@@ -1,2 +0,0 @@
-// Scrivere una terza versione del programma dell'esercizio 1 facendo uso delle system call
-// epoll_create/epoll_ctl/epoll_wait
